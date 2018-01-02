cc8fd4d00d306273b3055ad7ac1fef7ab88f2588
zhaosiyuan06 <15735800437@163.com>
2018-01-02 17:13:15 +0800

    ddd

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -14,16 +14,7 @@ $(function () {
     // $('.con1').on("mouseleave", function(){
     //     $(this).find('.con_box2').slideUp()
     // })
-    //监听鼠标滚动，设置导航栏背景色
-    $(window).scroll(function() {
-        var s =$(window).scrollTop();
-        if (s>=752) {//782是导航条离页面顶部的距离（px）
-            $(".top-header").addClass("colors")
-        }
-        else{
-            $('.top-header').removeClass('colors');
-        }
-    })
+
 $(".video_zhezhao img").on("click",function(){
     $(".video_zhezhao").css("display","none")
     $(".videoplay_Box video")[0].pause()
@@ -35,20 +26,43 @@ $(".video_zhezhao img").on("click",function(){
         $(".videoplay_Box video")[0].play()
     })
 
+//版本选项
+    $(".sale_sec_box .col-md-8 span").on("click",function(){
+        $(this).addClass("spans").siblings().removeClass("spans");
+    })
+    setInterval(function () {
+        setInterScroll()
+    }, 500)
+    function setInterScroll(){
+        //监听鼠标滚动，设置导航栏背景色
+        $(window).scroll(function() {
+            var s =$(window).scrollTop();
+            if (s>=50) {//782是导航条离页面顶部的距离（px）
+                $(".top_box").addClass("position_top")
+            }
+            else{
+                $('.top_box').removeClass('position_top');
+            }
+        })
+    }
 
 //    软件与服务
-    function xuan(TabBox,lis){
-        $(TabBox).on("mouseenter",function(){
-            var index =$(this).index();
-            $(lis).removeClass("active")
-            $(lis).eq(index).addClass("active").siblings().removeClass("active");
-        })
-        // $(TabBox).on("mouseleave",function(){
-        //     var index =$(this).index();
-        //     $(lis).eq(index).removeClass("active")
-        // })
-    }
-    xuan(".first li",".second ul")
-    xuan(".second ul li",".third ul")
+//     function xuan(TabBox,lis){
+//         $(TabBox).on("mouseenter",function(){
+//             var index =$(this).index();
+//             $(lis).removeClass("active")
+//             $(lis).eq(index).addClass("active").siblings().removeClass("active");
+//         })
+//         // $(TabBox).on("mouseleave",function(){
+//         //     var index =$(this).index();
+//         //     $(lis).eq(index).removeClass("active")
+//         // })
+//     }
+//     xuan(".first li",".second ul")
+//     xuan(".second ul li",".third ul")
 
+
+    // $(".first li").on("mouseenter",function(){
+    //     $(this).children(".second").addClass("active").siblings().removeClass("active");
+    // })
 })
